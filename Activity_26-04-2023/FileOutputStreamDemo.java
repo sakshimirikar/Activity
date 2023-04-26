7eb229ba3fdc89a2e8d3ad8fe21037e30cd5943b
sakshimirkar <78417855+sakshimirikar@users.noreply.github.com>
2023-04-26 19:27:27 +0530

    Create FileOutputStreamDemo.java

---
+++ Activity_26-04-2023/FileOutputStreamDemo.java
@@ -0,0 +1,34 @@
+
+package FileHandling;
+
+import java.io.*;
+
+//FileOutputSttream is used to write a file.
+
+public class FileOutputStreamDemo {
+
+	public static void main(String[] args) {
+		
+		try {
+			FileOutputStream fos = new FileOutputStream("D:/JAVA Training/Notes//demo.txt");
+			
+//			fos.write(65);
+			
+			String s = "Welcome to Java Stream";
+			byte b[] = s.getBytes(); //converting string into byte array
+			
+			fos.write(b);
+			fos.close();
+			
+			System.out.println("Success");
+			
+			
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+			
+		}
+	}
+
+}
+
