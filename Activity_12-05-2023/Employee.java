491e4abceb58931baf8cd077e02269d836dc568c
sakshimirkar <78417855+sakshimirikar@users.noreply.github.com>
2023-05-13 00:01:37 +0530

    Create Employee.java

---
+++ Activity_12-05-2023/Employee.java
@@ -0,0 +1,21 @@
+
+package com.Emp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Employee {
+	
+	private long empId;
+	private String empName;
+	private double empSal;
+	
+    Address address;
+	
+}
